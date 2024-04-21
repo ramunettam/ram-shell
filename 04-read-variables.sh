557280c91c9541a3f89adb4e95cf0a2a0ed514e8
ramunettam <ramunettam@gmail.com>
2024-04-21 09:46:12 +0200

    new changes

---
+++ 04-read-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "enter frist person name::"
+read PERSON1
+echo "enter second person name::"
+read PERSON2=$2
+echo "$PERSON1 ::Hi $PERSON2, how are you?"
+echo "$PERSON2::hello $PERSON1, i am good how are you?"
+echo "$PERSON1:: i am fine $PERSON2, what are you doing?"
+echo "$PERSON2:: i am planning to go home $PERSON2"S
