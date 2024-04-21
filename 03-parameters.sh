7543518e080540f3002308b33b8984ab6229a03b
ramunettam <ramunettam@gmail.com>
2024-04-21 09:40:18 +0200

    new changes

---
+++ 03-parameters.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=shyam$2
+
+echo "$PERSON1 ::Hi $PERSON2, how are you?"
+echo " $PERSON2::hello $PERSON1, i am good how are you?"
+echo "$PERSON1:: i am fine $PERSON2, what are you doing?"
+echo "$PERSON1:: i am planning to go home $PERSON2"S
