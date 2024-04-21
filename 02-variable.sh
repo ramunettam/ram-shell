b76bb04ab5749cdf656dd888d1ad217a246dc40c
ramunettam <ramunettam@gmail.com>
2024-04-21 09:38:32 +0200

    new changes

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-PERSON=RAMU
-PERSON1=shyam
+PERSON1=RAMU
+PERSON2=shyam
 
-echo "$person ::Hi $person1, how are you?"
-echo " $PERSON1::hello $PERSON, i am good how are you?"
+echo "$PERSON1 ::Hi $PERSON2, how are you?"
+echo " $PERSON2::hello $PERSON1, i am good how are you?"
+echo "$PERSON1:: i am fine $PERSON2, what are you doing?"
+echo "$PERSON1:: i am planning to go home $PERSON2"S
