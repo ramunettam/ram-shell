e2debff3489ae10fce802f6c51f014545207b5f2
ramunettam <ramunettam@gmail.com>
2024-04-21 09:23:17 +0200

    new changes

---
+++ 01-frist.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "RAMU ::Hi Ramu, how are you?"
+echo " SHAYAM::hello shyam, i am good how are you?"
+echo "RAMU:: i am good, are you going home?
+echo " SHAYAM:: no i have work so i am not going home"
